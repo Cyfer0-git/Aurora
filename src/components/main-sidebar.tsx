c766156b3e027159f459b01fad421fbde8053a10
Avay Gupta <avay.gupta@auroramy.com>
2025-10-22 09:35:24 +0000

    where i can't see admin access things

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -59,6 +59,7 @@ export function MainSidebar() {
   const { user } = useAuth();
 
   const getInitials = (name: string) => {
+    if (!name) return '';
     const names = name.split(' ');
     if (names.length > 1) {
       return `${names[0][0]}${names[names.length - 1][0]}`;

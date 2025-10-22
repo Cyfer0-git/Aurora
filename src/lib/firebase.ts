dc51b722cb88d56ff9287bb4f0f4d2b5ccf583e8
Avay Gupta <avay.gupta@auroramy.com>
2025-10-22 09:18:16 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCV_iL3fTew4Y-o_V3UO3b_J-i9kL_xL4w",
+  apiKey: "AIzaSyB3mtDcW_HHBgM11lmiNfKrLDKWg_W0tVo",
   authDomain: "gen-lang-client-0115875430.firebaseapp.com",
   projectId: "gen-lang-client-0115875430",
   storageBucket: "gen-lang-client-0115875430.appspot.com",
-  messagingSenderId: "1083442122687",
-  appId: "1:1083442122687:web:f5c3c3a0b4b4c7d8b5c9c7",
-  measurementId: "G-F385EH8J6H"
+  messagingSenderId: "60111589935",
+  appId: "1:60111589935:web:77f0d28417958199ffecda",
+  measurementId: "G-K8ZSD5L2XB"
 };
 
 // Initialize Firebase
